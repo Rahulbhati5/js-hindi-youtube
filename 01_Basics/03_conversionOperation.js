ab5890616ddb524897d91e740d7d50094a588ab5
rahulbhati5 <138871161+Rahulbhati5@users.noreply.github.com>
2026-01-01 15:10:07 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score = "33"
+//let score = "33abc" // NaN
+//let score = null; // 0
+//let score = undefined // Nan
+//let score = true // 1
+//let score = "Rahul" //NaN
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+
+//let isLoggedIn = 1 // true
+//let isLoggedIn = "" // false
+let isLoggedIn = "Rahul" //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rahul" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
