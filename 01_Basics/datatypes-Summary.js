11d1a6e9fcc9ef8038d86b1fe56fd0b02a945c5b
rahulbhati5 <138871161+Rahulbhati5@users.noreply.github.com>
2026-01-02 15:50:38 +0000

    stack and heap

--- 01_Basics/datatypes-Summary.js
+++ 01_Basics/datatypes-Summary.js
@@ -30,12 +30,36 @@ let myOBj = {
 }
 
 const myfunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
 //console.log(typeof bigNumber);
 
-console.log(typeof heros );
+// console.log(typeof heros );
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
 
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap(NoN-Primitive)
+
+let myYoutubename = "RahulBhatiDot.com"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "Rahul@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
