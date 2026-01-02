db95cc0e58eabe1b6b0d75e5c7d4ad1159369acf
rahulbhati5 <138871161+Rahulbhati5@users.noreply.github.com>
2026-01-02 05:29:01 +0000

    increment and decrement

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -30,7 +30,54 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ******************* Operations ************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+/*console.log(2+2);
+console.log(3-3);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+let str1 = "Rahul"
+let str2 = " bhati"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+//console.log(+true); //1
+//console.log(true+); //error
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //should not proper industry way
+
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html
+
+
+
+
+
 
 
