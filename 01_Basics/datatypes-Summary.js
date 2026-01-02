4c6331c6acd3f11d3c3dfd2fcab65dbf89db1847
rahulbhati5 <138871161+Rahulbhati5@users.noreply.github.com>
2026-01-02 15:22:57 +0000

    datatypes-summary

---
+++ 01_Basics/datatypes-Summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp =  null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 2134134314142343242344324234234324n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects(if you want to master in js focus on this), Functions
+
+const heros = ["shaKtiman", "naagraj", "doga"];
+let myOBj = {
+    name: "rahul",
+    age: 24,
+
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+//console.log(typeof bigNumber);
+
+console.log(typeof heros );
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
