f893ea44b6f03e92aeadba965f6b552bc42822c8
rahulbhati5 <138871161+Rahulbhati5@users.noreply.github.com>
2026-01-05 06:49:29 +0000

    object_function_array

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -32,4 +32,33 @@ function loginUserMessage(username = "sam"){
 } 
 
 // console.log(loginUserMessage("Rahul"))
-console.log(loginUserMessage("Rahul"))
+// console.log(loginUserMessage("Rahul"))
+
+function calculateCartPrice(val1, val2, ...num1){
+     return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+     username: "hitesh",
+     prices: 199
+}
+function handleObject(anyObeject){
+     console.log(`Username is ${anyObeject.username} and price is ${anyObeject.price}`);
+
+}
+// handleObject(user)
+handleObject({
+     username: "sam",
+     price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+     return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 1000]));
